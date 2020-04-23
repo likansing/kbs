daa1c45b38fa5c88101453c5b4d6d3bd2e1eee71
du_ma <du_ma@DESKTOP-C1CHSFR>
2020-04-23 00:25:22 -0300

    Rev3_April23_2020
    -kbs/src/main/resources/templates/register/registerknowledge.html:
    *alterado campos de data para type=date eremovido class=datepicker
    *removido js datepicker junction
    *pacote com.kbs.config foi adicionado com 2 classes porem nao usadas,
    logo comentadas.

--- kbs/src/main/java/com/kbs/KbsApplication.java
+++ kbs/src/main/java/com/kbs/KbsApplication.java
@@ -3,6 +3,7 @@ package com.kbs;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -12,6 +13,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @ComponentScan(basePackages = {"com.*"})
 @EnableJpaRepositories(basePackages = {"com.kbs.repository"})
 @EnableTransactionManagement
+//@ConfigurationPropertiesScan(basePackages = {"com.*"})
 public class KbsApplication {
 
 	public static void main(String[] args) {
--- kbs/src/main/java/com/kbs/controller/KnowledgeController.java
+++ kbs/src/main/java/com/kbs/controller/KnowledgeController.java
@@ -11,9 +11,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
@@ -56,6 +54,8 @@ public class KnowledgeController {
 	
 	@Autowired
 	DinamicHql dinamicHql;
+	
+
 
 	@RequestMapping("/")
 	public ModelAndView index() {
